2ebbcf5111fe8536ff6874f3983a63746329c2eb
OrTab <or.1532.tabin@gmail.com>
2024-01-21 23:28:06 +0200

    extend socket ability for testing

--- client/src/services/SocketService.ts
+++ client/src/services/SocketService.ts
@@ -1,6 +1,18 @@
 class SocketService {
   private socket: WebSocket | null = null;
-  connect() {}
+  private isConnected = false;
+  connect() {
+    this.socket = new WebSocket('ws://127.0.0.1:4001/socket');
+    this.socket.addEventListener('open', (ev) => {
+      this.isConnected = true;
+      console.log('Socket connected');
+      this.socket!.send('Hey server');
+    });
+
+    this.socket.addEventListener('message', (ev) => {
+      console.log('Received message', ev);
+    });
+  }
 }
 
 export default new SocketService();
