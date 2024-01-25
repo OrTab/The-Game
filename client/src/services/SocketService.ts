6e1024a4b3b49f03e63ab734e47ae0b64a525154
OrTab <or.1532.tabin@gmail.com>
2024-01-25 23:06:19 +0200

    Implement mechanism for handling action before connection

--- client/src/services/SocketService.ts
+++ client/src/services/SocketService.ts
@@ -1,9 +1,10 @@
-import { SocketData } from './../../../shared/socket.types.d';
+import { SocketEvent } from './../../../shared/socket.types.d';
 import { EventBus } from '../../../shared/EventBus';
 
 class SocketService {
   private socket!: WebSocket;
   private isConnected = false;
+  private wasConnected = false;
   private eventBus: EventBus;
 
   constructor() {
@@ -16,12 +17,13 @@ class SocketService {
     this.socket = new WebSocket('ws://127.0.0.1:4001/socket');
     this.socket.addEventListener('open', () => {
       this.isConnected = true;
+      this.wasConnected = true;
       console.log('Socket connected');
     });
 
     this.socket.addEventListener('message', (ev) => {
       const { data } = ev;
-      const { data: eventData, eventName }: SocketData = JSON.parse(data);
+      const { data: eventData, eventName }: SocketEvent = JSON.parse(data);
       this.eventBus.emit(eventName, eventData);
     });
 
@@ -31,15 +33,31 @@ class SocketService {
     });
   }
 
-  on(eventName: string, cb: (...args: any) => void) {
-    this.eventBus.on(eventName, cb);
+  private handleSocketNotLoaded(callback: () => void) {
+    if (!this.isConnected && !this.wasConnected) {
+      setTimeout(callback, 100);
+    } else {
+      callback();
+    }
   }
 
-  emit(eventName: string, data: SocketData['data']) {
-    if (!this.isConnected) {
-      return;
-    }
-    this.socket.send(JSON.stringify({ eventName, data }));
+  on(eventName: string, callback: (...args: any) => void) {
+    const action = () => {
+      this.eventBus.on(eventName, callback);
+      this.socket.send(
+        JSON.stringify(<SocketEvent>{ eventName, type: 'subscribe' })
+      );
+    };
+    this.handleSocketNotLoaded(action);
+  }
+
+  emit(eventName: string, data: SocketEvent['data']) {
+    const action = () => {
+      this.socket.send(
+        JSON.stringify(<SocketEvent>{ type: 'emit', eventName, data })
+      );
+    };
+    this.handleSocketNotLoaded(action);
   }
 }
 
