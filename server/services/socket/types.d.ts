bd8c38163f80af96fa9932bc2709868747d26814
OrTab <or.1532.tabin@gmail.com>
2024-01-25 05:03:37 +0200

    improve typing

--- server/services/socket/types.d.ts
+++ server/services/socket/types.d.ts
@@ -1,8 +1,13 @@
 import type { Socket as OriginalSocket } from 'net';
+import type { EventBus } from '../../../shared/EventBus';
 export interface Socket extends OriginalSocket {
   id: string;
   emitEvent: (eventName: string, data: any) => void;
   send: (data: any) => boolean;
-  sub(eventName: string, cb: <T = any>(data: any) => void);
-  broadcast(eventName: string, data: any);
+  sub: (
+    eventName: string,
+    cb: <T = any>(data: any) => void
+  ) => ReturnType<EventBus['on']>;
+  broadcast: (eventName: string, data: any) => void;
+  eventBus: EventBus;
 }
